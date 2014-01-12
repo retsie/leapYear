d9ee1ca7c0beaa4d1b2cbd68a16c4451123eb814
retsie <e1st3r77@gmail.com>
2014-01-13 04:09:42 +0800

    Update years.php

--- js/years.php
+++ js/years.php
@@ -2,32 +2,27 @@
 $year = $_GET['years'];
 $month = $_GET['months'];
 $days = 0;
+
 	if(($year % 4)==0){
-		if (in_array($month, array('jan', 'mar', 'may','jul', 'aug', 'oct', 'dec'))) {
-			$days = 31;
-		} elseif (in_array($month, array('apr', 'jun', 'sep', 'nov'))) {
-			$days = 30;
-		}  elseif (in_array($month, array('feb'))) {
+		if (in_array($month, array('feb'))) {
 			$days = 29;
 		}
 	}else{
-		if (in_array($month, array('jan', 'mar', 'may','jul', 'aug', 'oct', 'dec'))) {
-			$days = 31;
-		} elseif (in_array($month, array('apr', 'jun', 'sep', 'nov'))) {
-			$days = 30;
-		}  elseif (in_array($month, array('feb'))) {
+		if (in_array($month, array('feb'))) {
 			$days = 28;
 		}
+	}if (in_array($month, array('jan', 'mar', 'may','jul', 'aug', 'oct', 'dec'))) {
+			$days = 31;
+	}elseif (in_array($month, array('apr', 'jun', 'sep', 'nov'))) {
+			$days = 30;
 	}
-
+	
 if ($days !== false) {
-	echo json_encode(
-		array('years' => $year,
-			  'months' => $month,
-			  'days'	=> $days,
-			  'result' => 'success'));
+	echo json_encode(array('years' => $year,
+			       'months' => $month,
+			       'days'	=> $days,
+			       'result' => 'success'));
 } else {
-	echo json_encode(
-		array('months' => $month,
-			  'result' => 'failed'));
+	echo json_encode(array('months' => $month,
+			       'result' => 'failed'));
 }
